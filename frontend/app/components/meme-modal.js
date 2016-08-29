9b6ad0b1fae6ccfb6b86f370e012d1cf700c95eb
Oskar Niburski <oskarniburski@gmail.com>
2016-08-29 16:20:18 -0400

    Trivial modal

--- frontend/app/components/meme-modal.js
+++ frontend/app/components/meme-modal.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   tagName: 'div',
   actions: {
     toggleModal() {
-      this.set('isModalVisible', false);
+      this.toggleProperty('isModalVisible');
     }
   }
 });
--- frontend/app/controllers/memes/index.js
+++ frontend/app/controllers/memes/index.js
@@ -3,10 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['title'],
   title: "",
+  isModalVisible: false,
   actions: {
-    toggleModal(meme) {
-      console.log('making meme modal visible');
-      this.set('isModalVisible', true);
-    }
+    // toggleModal(meme) {
+    //   console.log('making meme modal visible');
+    //   this.set('isModalVisible', true);
+    // }
   }
 });
