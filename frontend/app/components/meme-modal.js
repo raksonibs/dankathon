bf4e77fa8398dfd4f6680a35da7ebaff94c84f92
Oskar Niburski <oskarniburski@gmail.com>
2016-08-29 17:57:27 -0400

    Hilarious animation

--- frontend/app/components/meme-modal.js
+++ frontend/app/components/meme-modal.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['meme-modal'],
   tagName: 'div',
+  animated: Ember.computed('isModalVisible', function() {
+    return this.get('isModalVisible') === true ? 'active' : '';
+  }),
   actions: {
     toggleModal() {
       this.toggleProperty('isModalVisible');
