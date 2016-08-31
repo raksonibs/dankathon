367c00aaa040b77845047646e46fdd2e214e25a7
Oskar Niburski <oskarniburski@gmail.com>
2016-08-31 09:50:29 -0400

    Trump consume when want wall select

--- frontend/app/components/trump-consume.js
+++ frontend/app/components/trump-consume.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showTrumps() {
+    let tCont = Ember.$('.trumps-everywhere')[0];
+    var img;
+
+    for (var i = 0; i < 100; i++) {
+        setTimeout(function() {
+          img = document.createElement('img');
+          img.src = '/assets/trump-askance.png';
+          img.style.display = "fixed";
+          img.className = "rand-trump"
+          img.style.top = (Math.random() * 100) + "%";
+          img.style.left = (Math.random() * 100) + "%";
+          tCont.appendChild(img);
+        }, 1000 - (Math.pow(2, i)));
+    }
+
+  },
+
+  didUpdate() {
+    this.showTrumps();
+  }
 });
--- frontend/app/routes/dank.js
+++ frontend/app/routes/dank.js
@@ -45,38 +45,12 @@ export default Ember.Route.extend({
 
       let dankTextSet = dankText.target.value;
 
-      if (/dank/.exec(dankTextSet)) {
-        this.controller.set('super-dank', true);
-      } else {
-        this.controller.set('super-dank', false);
-      }
-
-      if (/cat/.exec(dankTextSet)) {
-        this.controller.set('rotate', true);
-      } else {
-        this.controller.set('rotate', false);
-      }
-
       if ('trump' === dankTextSet || dankTextSet.match(/trump/g).length >= 2) {        
         var outcome = trumpGame.setTrumpCount(dankTextSet.match(/trump/g).length);
         if (outcome !== false) {          
           this.controller.set(outcome["key"], outcome["value"]);
-        }
+        }        
       }
-
-      // if (/darude/.exec(dankTextSet)) {        
-      //   darude.playSong();
-      // } else {
-      //   darude.stopSong();
-      // }
-
-      // if (/trump/.exec(dankTextSet)) {        
-      //   trump.playSong();
-      // } else {
-      //   trump.stopSong();
-      // }
-
-
     }
   }
 });
